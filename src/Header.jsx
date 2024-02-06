e992a36d2623d129949730d21975a9857e7843ac
raj shringi <raj24shringi@gmail.com>
2024-02-06 23:21:11 +0530

    create Header desktop UI

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,21 @@
+import MenuKebab from "./assets/MenuKebab.svg";
+import kanbanLightImage from "./assets/kanban-light-logo.svg";
+
 function Header() {
   return (
-    <div className="bg-[#f4f7fd] flex justify-between py-6">
-      <div className="flex-[15%]">Kanban</div>
-      <div className="flex-[85%] flex justify-between">
-        <h2>Example Board</h2>
-        <div className="space-x-4">
-          <button>+Add new task</button>
-          <button>icon</button>
+    <div className="bg-white flex  items-stretch">
+      <div className="flex-[18%] flex items-center max-w-[300px] px-6  border-r border-r-[#e4ebfa]">
+        <img src={kanbanLightImage} alt="kanban-logo" />
+      </div>
+      <div className="flex-[82%] flex justify-between items-center px-4 py-6">
+        <h2 className="font-bold text-2xl">Platform Launch</h2>
+        <div className="space-x-4 flex items-center">
+          <button className="bg-[#635fc7] hover:bg-[#635fc8c9] text-white px-6 py-3 rounded-full font-bold">
+            + Add new task
+          </button>
+          <button>
+            <img src={MenuKebab} alt="MenuKebab" />
+          </button>
         </div>
       </div>
     </div>
