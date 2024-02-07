6097f8208949e5e505287f84b198e43fef827c11
raj shringi <raj24shringi@gmail.com>
2024-02-07 22:57:37 +0530

    use react icons instead of svg image

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,20 +1,20 @@
-import MenuKebab from "./assets/MenuKebab.svg";
+import { MdOutlineMoreVert } from "react-icons/md";
 import kanbanLightImage from "./assets/kanban-light-logo.svg";
 
 function Header() {
   return (
     <div className="bg-white flex  items-stretch">
-      <div className="flex-[18%] flex items-center max-w-[300px] px-6  border-r border-r-[#e4ebfa]">
+      <div className="flex-[20%] flex items-center max-w-[300px] px-6  border-r border-r-[#e4ebfa]">
         <img src={kanbanLightImage} alt="kanban-logo" />
       </div>
-      <div className="flex-[82%] flex justify-between items-center px-4 py-6">
+      <div className="flex-[80%] flex justify-between items-center px-4 py-6">
         <h2 className="font-bold text-2xl">Platform Launch</h2>
         <div className="space-x-4 flex items-center">
           <button className="bg-[#635fc7] hover:bg-[#635fc8c9] text-white px-6 py-3 rounded-full font-bold">
             + Add new task
           </button>
           <button>
-            <img src={MenuKebab} alt="MenuKebab" />
+            <MdOutlineMoreVert className="text-2xl text-gray-400" />
           </button>
         </div>
       </div>
