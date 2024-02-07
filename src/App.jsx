c54f5544ed6bffb6215c8582bf1cbf774cb4e3c6
raj shringi <raj24shringi@gmail.com>
2024-02-07 23:03:31 +0530

    change width of sidebar and board to make look more like given design

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Header from "./Header";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen  ">
+    <div className="flex flex-col min-h-screen bg-[#f4f7fd] text-gray-900">
       <Header />
       <div className="flex flex-1">
-        <div className="flex-[15%]">
+        <div className="flex-[20%] max-w-[300px]">
           <Sidebar />
         </div>
-        <div className="flex-[85%]">
+        <div className="flex-[80%]">
           <Board />
         </div>
       </div>
