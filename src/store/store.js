af53f43c841a26e00ffc5d14f0463fd5d57aab8b
raj shringi <raj24shringi@gmail.com>
2024-04-23 12:46:46 +0530

    add userReducer and boardReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from "../slice/modalSlice";
+import userReducer from "../slice/userSlice";
+import boardReducer from "../slice/boardSlice";
+
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
+    user: userReducer,
+    board: boardReducer,
   },
 });
