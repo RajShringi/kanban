b996d97b8fe261be162fc225aab76838a7ebd481
raj shringi <raj24shringi@gmail.com>
2024-05-05 16:58:19 +0530

    handle click new column button

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,15 +1,24 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Column from "./Column";
+import { open } from "./slice/modalSlice";
 
 function Board() {
   const { activeBoard } = useSelector((state) => state.board);
   const columns = activeBoard?.columns;
+  const dispatch = useDispatch();
+  function handleNewColumnClick() {
+    dispatch(open({ modal: "editBoard" }));
+  }
+
   return (
-    <div className={`bg-[#e4ebfa] p-4 min-w-max flex flex-grow gap-4`}>
+    <div className={`bg-[#e4ebfa] p-4 min-w-max flex gap-4`}>
       {!columns
         ? ""
         : columns.map((column) => <Column key={column._id} column={column} />)}
-      <button className="flex w-[280px] py-2 shrink-0 mt-9 bg-[#f1f5fc] justify-center items-center font-bold text-2xl text-gray-500 rounded-md transition-all hover:scale-90">
+      <button
+        onClick={handleNewColumnClick}
+        className="flex w-[280px] py-2 shrink-0 mt-9 bg-[#f1f5fc] justify-center items-center font-bold text-2xl text-gray-500 rounded-md transition-all hover:scale-90"
+      >
         + New Column
       </button>
     </div>
