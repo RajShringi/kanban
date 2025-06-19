f27c3cfbad12118c72fd5abcd4c4b3de37956c4e
Raj Shringi <raj24shringi@gmail.com>
2025-06-19 17:32:12 +0530

    adjust loader to show in the middle of the screen

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader({ asButton }) {
   return (
-    <div>
+    <div className="flex items-center justify-center flex-1">
       <div className={`"text-center ${asButton ? "m-0" : "my-4"}"`}>
         <div className="animate-spin w-8 h-8 border-4 rounded-full inline-block  border-t-transparent border-indigo-400"></div>
       </div>
